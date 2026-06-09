28d0c9055b880bb1817af75cba98685cec516557
Neelkumar Patel <Neelkumar.2206@gmail.com>
2026-06-09 09:02:21 -0230

    test: verify useWeather hook with hardcoded Toronto data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import useWeather from "./hooks/useWeather";
 function App() {
+    const { data, loading, error } = useWeather(
+        "toronto",
+        "2026-04-01",
+        "2026-04-07",
+    );
+    console.log(data, loading, error);
     return <></>;
 }
 
