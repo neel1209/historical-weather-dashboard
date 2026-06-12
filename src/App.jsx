f8d40f6c91badc21492c66147dc3a6df1638d911
Neelkumar Patel <Neelkumar.2206@gmail.com>
2026-06-12 08:31:46 -0230

    feat(components): add SearchBar component with city and date range inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import SearchBar from "./components/Searchbar/SearchBar";
 function App() {
-    const onSearch = (cityName, startDate, endDate) => {
-        console.log(cityName, startDate, endDate);
-    };
-    return <SearchBar onSearch={onSearch} />;
+    return <></>;
 }
 
 export default App;
