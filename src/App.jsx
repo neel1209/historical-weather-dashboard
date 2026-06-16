af765c63d83a0a1b7f24cf6f73e47ff376c48015
Neelkumar Patel <Neelkumar.2206@gmail.com>
2026-06-16 00:59:37 -0230

    feat(components): add WeatherChart component with dual line chart for min/max values

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import useWeather from "./hooks/useWeather";
-import { dataFormatter } from "./utils/formatters";
-import WeatherChart from "./components/WeatherChart/WeatherChart";
 function App() {
-    const { data, loading, error } = useWeather(
-        "toronto",
-        "2026-04-15",
-        "2026-04-21",
-    );
-    const formatedData = data
-        ? dataFormatter(
-              data.dateRange,
-              data.maxTemperature,
-              data.minTemperature,
-          )
-        : [];
-
-    return <WeatherChart data={formatedData} title="Temperature" unit="°C" />;
+    return <></>;
 }
 
 export default App;
